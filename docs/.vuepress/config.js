bbd95adee850c531c0c050817536708417fb4328
alinagts <godovanetsalina@gmail.com>
2023-01-10 11:17:03 +0200

    naming improvements

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,8 @@
 // .vuepress/config.js
 module.exports = {
   plugins: [
-    '@vuepress/back-to-top', 
-    '@vuepress/pwa', 
+    '@vuepress/back-to-top',
+    '@vuepress/pwa',
     {
       serviceWorker: true,
       updatePopup: true
@@ -22,50 +22,50 @@ module.exports = {
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }],
     ['link', {href: 'https://fonts.googleapis.com/icon?family=Material+Icons', rel :'stylesheet'}],
-    
+
   ],
   themeConfig: {
     // logo: './myAvatar.png',
     sidebar: [
-    {
-      title: 'Вступ',
-      path:"/intro/"
-    },
-    {
-      title: 'Розроблення загальних вимог до системи',
+      {
+        title: 'Вступ',
+        path:"/intro/"
+      },
+      {
+        title: 'Розроблення загальних вимог до системи',
         path:"/requirements/",
         children:[
           '/requirements/state-of-the-art',
           '/requirements/stakeholders-needs',
         ]
-      
-    },
 
-    {
-      title: 'Розроблення вимог до функціональности системи',
-      path:"/use cases/"
-    },
+      },
 
-    {
-      title: 'Проектування інформаційного забезпечення',
-      path:"/design/"
-    },
+      {
+        title: 'Розроблення вимог до функціональности системи',
+        path:"/use cases/"
+      },
 
-    {
-      title: 'Реалізація інформаційного та програмного забезпечення',
-      path:"/software/"
-    },
+      {
+        title: 'Проектування інформаційного забезпечення',
+        path:"/design/"
+      },
 
-    {
-      title: 'Тестування працездатності системи',
-      path:"/test/"
-    },
-    {
-      title: 'Висновки',
-      path:"/conclusion/"
-    }
+      {
+        title: 'Реалізація інформаційного та програмного забезпечення',
+        path:"/software/"
+      },
 
-  ],
+      {
+        title: 'Тестування працездатності системи',
+        path:"/test/"
+      },
+      {
+        title: 'Висновки',
+        path:"/conclusion/"
+      }
+
+    ],
     sidebarDepth: 2,
     displayAllHeaders: true, // Default: false
 
@@ -75,7 +75,7 @@ module.exports = {
     ],
 
     lastUpdated: 'Останнє оновлення', // string | boolean
-      // Assumes GitHub. Can also be a full GitLab url.
+    // Assumes GitHub. Can also be a full GitLab url.
     repo: 'https://github.com/alinagts/media-content-analyze-system',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
@@ -96,7 +96,7 @@ module.exports = {
 
   },
   title: 'Система аналізу медіа-контенту',
-  description: 'Лабораторні роботи з дисципліни "Бази даних"',
+  description: 'Курсова робота з дисципліни "Бази даних"',
   configureWebpack: {
     resolve: {
       alias: {
